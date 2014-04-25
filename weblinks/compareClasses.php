9b603af007221bf0ca2f8e8b322f736d169a5d8b
Sonia <prodigalstudent@gmail.com>
2014-04-24 20:32:32 -0500

    this finally works

--- weblinks/compareClasses.php
+++ weblinks/compareClasses.php
@@ -86,21 +86,28 @@ foreach($_POST['class'] as $value)
     if(isset($_POST["submit"])){
 
             ?>
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 
-    <script type='text/javascript'>
+        <script type='text/javascript'>
         google.load('visualization', '1', {packages:['table']});
         google.setOnLoadCallback(drawTable);
         function drawTable() {
             var data = new google.visualization.DataTable();
             data.addColumn('string', 'Name');
             data.addColumn('string', 'Code');
+            data.addColumn('string', 'Credit');
+            data.addColumn('number','average difficulty' );
+            data.addColumn('number','average time');
+            data.addColumn('number', 'average enjoyment');
 
             data.addRows([
                 <? for($i=0; $i<count($classes); $i++):
                     if($i!=(count($classes)-1)):?>
-                        ['<? echo $classes[$i]->getTitle()?>', '<? echo $classes[$i]->getCode()?>'],
+                        ['<? echo $classes[$i]->getTitle()?>', '<? echo $classes[$i]->getCode()?>', '<? $classes[$i]->getCredit()?>',
+                        <? echo $diffAvgs[$i]?>, <? echo $timeAvgs[$i]?>, <? echo $enjAvgs[$i]?>],
                     <? else:?>
-                        ['<? echo $classes[$i]->getTitle()?>', '<? echo $classes[$i]->getCode()?>']
+                        ['<? echo $classes[$i]->getTitle()?>', '<? echo $classes[$i]->getCode()?>', '<? $classes[$i]->getCredit()?>',
+                            <? echo $diffAvgs[$i]?>, <? echo $timeAvgs[$i]?>, <? echo $enjAvgs[$i]?>]
                     <?
                     endif;
                    endfor;?>
